0dbff688ea25e7b1b457d2a729134ec9738354f0
nuthanmurarysetty <inbox.nuthan@gmail.com>
2025-07-03 16:20:21 +0530

    Update API base URL in index.html and enable API_BASE_URL in scripts for production deployment

--- backend/server.js
+++ backend/server.js
@@ -14,18 +14,18 @@ import cors from "cors";
 import routes from "./routes/index.js";
 import { join } from "path"; //- For local static file serving
 import { fileURLToPath } from "url";
-import { dirname } from "path";
+// import { dirname } from "path";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 // ✅ Must be first: Health check
-// app.get("/", (req, res) => {
-//   res.status(200).send("✅ API backend is running");
-// });
+app.get("/", (req, res) => {
+  res.status(200).send("✅ API backend is running");
+});
 
 // ✅ Middlewares
 app.use(cors());
@@ -34,7 +34,7 @@ app.use(urlencoded({ extended: true }));
 
 // ✅ Static files - For Local testing
 // Serve static files from the "src" directory
-app.use(expressStatic(join(__dirname, "../src")));
+// Todo: app.use(expressStatic(join(__dirname, "../src")));
 
 // ✅ Routes
 app.use("/api", routes);
